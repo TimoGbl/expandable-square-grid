dfdf0afd3caedcda3a1761aae88ecdb008f67a9d
Timo Göbel <TimoGbl@users.noreply.github.com>
2015-10-18 19:04:05 +0200

    Add comments

--- scripts/script.js
+++ scripts/script.js
@@ -6,9 +6,11 @@ for (var i = 0; i < squares.length; i++) {
 
 function open(element) {
     return function() {
+        // remove active class from all square
         for (var i = 0; i < squares.length; i++) {
             squares[i].classList.remove('active');
         }
+        // add class active to target
         element.classList.add('active');
     };
 }
