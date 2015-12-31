b9e38ed1b8ffa040e72e873a4aa5baa3f7b160cf
Timo Göbel <timo@codeatelier.com>
2015-12-31 15:35:11 +0100

    Split trabsition declarations

--- scripts/expandable-grid.js
+++ scripts/expandable-grid.js
@@ -1,6 +1,6 @@
 var ExpandableGrid = (function() {
   "use strict"
-  
+
   // Data
   var list = {};
   var texts = [];
@@ -9,38 +9,38 @@ var ExpandableGrid = (function() {
     duration: 0.5,
     timing: 'ease-in-out'
   };
-  
+
   // State
   var expanded = false;
-  
+
   // Constructor
   var Init = function(element, transitionInput) {
- 	
+
  	// Save transition information
  	if (transitionInput) {
  	    transitionProps = transitionInput;
  	}
- 	
+
  	// Get html list
   	list = document.getElementById(element);
-  	
-  	// Set css transition property
-  	list.style.transition = transitionProps.properties +
-  	    ' ' + transitionProps.duration +
-  	    ' ' + transitionProps.timing;
-  	
+
+  	// Set css transition properties
+  	list.style.transitionProperty = transitionProps.properties;
+    list.style.transitionDuration = transitionProps.duration;
+    list.style.transitionTimingFunction = transitionProps.timing;
+
   	// Save texts and hide original
   	for (var i = 0; i < list.children.length; i++) {
   		var listElement = list.children[i];
-  		
+
   		// Save detail texts
   		texts.push(listElement.children[1].innerHTML);
-  		
+
   		// Hide detail texts
   		listElement.children[1].style.display ='none';
   	}
   };
-  
+
   return function(element, transitionInput) {
     new Init(element, transitionInput);
   };
