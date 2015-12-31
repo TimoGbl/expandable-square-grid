242c3722c9343033d9532896072ad8dd033964d7
Timo Göbel <TimoGbl@users.noreply.github.com>
2015-12-31 12:20:20 +0100

    Add transition API

--- scripts/expandable-grid.js
+++ scripts/expandable-grid.js
@@ -1,11 +1,27 @@
 var ExpandableGrid = (function() {
   "use strict"
   
-  var list;
+  // Data
+  var list = {};
   var texts = [];
+  var transitionProps = {
+    properties: 'all',
+    duration: 0.5,
+    timing: 'ease-in-out'
+  };
+  
+  // State
+  var expanded = false;
   
   // Constructor
-  var Init = function(element) {
+  var Init = function(element, transitionInput) {
+ 	
+ 	// Save transition information
+ 	if (transitionInput) {
+ 	    transitionProps.properties = transitionInput.properties;
+ 	    transitionProps.duration = transitionInput.duration;
+ 	    transitionProps.timing = transitionInput.timing;
+ 	}
  	
  	// Get html list
   	list = document.getElementById(element);
@@ -18,7 +34,7 @@ var ExpandableGrid = (function() {
   	}
   };
   
-  return function(element) {
-  	new Init(element);
+  return function(element, transitionInput) {
+    new Init(element, transitionInput);
   };
  })();
