53b51126edb993b35648a2d598d3475441a79ec6
Timo Göbel <timo@codeatelier.com>
2016-01-17 14:27:02 +0100

    Add expandable element

--- scripts/expandable-grid.js
+++ scripts/expandable-grid.js
@@ -1,47 +1,106 @@
 var ExpandableGrid = (function() {
-  "use strict"
+    'use strict';
 
-  // Data
-  var list = {};
-  var texts = [];
-  var transitionProps = {
-    properties: 'all',
-    duration: 0.5,
-    timing: 'ease-in-out'
-  };
+    // Data
+    var list = {};
+    var heights = [];
+    var expandetSquare = {};
+    var texts = [];
+    var transitionProps = {
+        properties: 'height',
+        duration: 0.5,
+        timing: 'cubic-bezier(0.5, 0.1, 0, 1)'
+    };
 
-  // State
-  var expanded = false;
+    // State
+    var expanded = false;
 
-  // Constructor
-  var Init = function(element, transitionInput) {
+    function toggleSquare(index) {
+        if (expanded) {
+            expandetSquare.children[0].innerHTML = texts[index];
+            expandetSquare.style.height = heights[index] + 'px';
+        } else {
+            expandetSquare.style.height ='0';
+        }
+    }
 
- 	// Save transition information
- 	if (transitionInput) {
- 	    transitionProps = transitionInput;
- 	}
+    // Click list element
+    function click(element, index) {
+        return function() {
+            // Save if element was active before
+            var elementActive = element.classList.contains('active') ? true : false;
 
- 	// Get html list
-  	list = document.getElementById(element);
+            // remove active class from all square
+            for (var i = 0; i < list.children.length; i++) {
+                list.children[i].classList.remove('active');
+            }
 
-  	// Set css transition properties
-  	list.style.transitionProperty = transitionProps.properties;
-    list.style.transitionDuration = transitionProps.duration;
-    list.style.transitionTimingFunction = transitionProps.timing;
+            if (!elementActive) {
+                // add class active to target
+                element.classList.add('active');
+                expanded = true;
+            } else {
+                expanded = false;
+            }
 
-  	// Save texts and hide original
-  	for (var i = 0; i < list.children.length; i++) {
-  		var listElement = list.children[i];
+            toggleSquare(index);
+        };
+    }
 
-  		// Save detail texts
-  		texts.push(listElement.children[1].innerHTML);
+    // Constructor
+    var Init = function(element, transitionInput) {
 
-  		// Hide detail texts
-  		listElement.children[1].style.display ='none';
-  	}
-  };
+        var i;
 
-  return function(element, transitionInput) {
-    new Init(element, transitionInput);
-  };
- })();
+        // Save transition information
+        if (transitionInput) {
+            transitionProps = transitionInput;
+        }
+
+        // Get html list
+        list = document.getElementById(element);
+
+        // Save texts and hide original
+        for (i = 0; i < list.children.length; i++) {
+            var listElement = list.children[i];
+
+            // Save detail texts
+            texts.push(listElement.children[1].innerHTML);
+
+            // Hide detail texts
+            listElement.children[1].style.display ='none';
+
+            // Add event listeners
+            listElement.addEventListener("click", click(listElement, i));
+        }
+
+        // Create new li
+        expandetSquare = document.createElement('div');
+
+        expandetSquare.style.width ='100%';
+
+        expandetSquare.style.transition = transitionProps.properties + ' ' +
+            transitionProps.timing + ' ' +
+            transitionProps.duration + 's';
+
+        expandetSquare.setAttribute('class', 'expandetElement');
+
+        var newP = document.createElement('p');
+        var textnode = document.createTextNode('Detail container');
+
+        newP.appendChild(textnode);
+        expandetSquare.appendChild(newP);
+        list.insertBefore(expandetSquare, list.childNodes[list.children.length]);
+
+        for (i = 0; i < texts.length; i++) {
+            expandetSquare.children[0].innerHTML = texts[i];
+            heights.push(expandetSquare.clientHeight);
+        }
+        expandetSquare.children[0].innerHTML = '';
+        expandetSquare.style.height = '0';
+    };
+
+    return function(element, transitionInput) {
+        new Init(element, transitionInput);
+    };
+})();
